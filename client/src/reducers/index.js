122a8f1f571863c40a4cb0392c72eb1025ad5c33
Tammie Chung <tammie.chung11@gmail.com>
2020-01-09 17:21:14 -0500

    Begin looking into Levenshtein distance for fuzzy string matching

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -6,6 +6,49 @@ const initialState = {
   filteredStats: [],
 };
 
+// const levenshtein = (searchTerm, source) => {
+  
+//   if (searchTerm.length === 0) {
+//     return source.length;
+//   }
+
+//   if (source.length === 0) {
+//     return searchTerm.length;
+//   }
+
+//   let matrix = [];
+//   let i;
+//   let j;
+
+//   // increment along the first column of each row
+//   for (i = 0; i <= source.length; i++){
+//     console.log('matrix i:', matrix)
+//     matrix[i] = [i];
+//   }
+
+//   // increment each column in the first row
+//   for (j = 0; j <= searchTerm.length; j++){
+//     console.log('matrix j:', matrix)
+//     matrix[0][j] = j;
+//   }
+
+//   // Fill in the rest of the matrix
+//   for (i = 1; i <= source.length; i++){
+//     for (j = 1; j <= searchTerm.length; j++){
+//       if(source.charAt(i - 1) === searchTerm.charAt(j - 1)){
+//         matrix[i][j] = matrix[i - 1][j - 1];
+//       } else {
+//         matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, // substitution
+//           Math.min(matrix[i][j - 1] + 1, // insertion
+//           matrix[i - 1][j] + 1)); // deletion
+//       }
+//     }
+//   }
+
+//   return matrix[source.length][searchTerm.length];
+
+// };
+
 function rootReducer(state = initialState, action) {
   
   if (action.type === GET_HOURLY_EVENTS) {
@@ -17,7 +60,10 @@ function rootReducer(state = initialState, action) {
   }
 
   if (action.type === FILTER_HOURLY_STATS) {
-    const filteredStats = state.hourlyStats.filter(stat => stat.location.includes(action.payload))
+    const filteredStats = state.hourlyStats.filter(stat => {
+      // console.log('lev', levenshtein(action.payload.toLowerCase(), stat.location.toLowerCase()));
+      return stat.location.toLowerCase().includes(action.payload.toLowerCase());
+    });
     return {...state, filteredStats: [...filteredStats] }
   }
 
