f7089d9b555a1c173117dc8d4386bef884598c03
Tammie Chung <tammie.chung11@gmail.com>
2020-01-09 16:02:46 -0500

    Merge API and Client-side code into one repository

---
+++ api/index.js
@@ -0,0 +1,98 @@
+require('dotenv').config();
+
+const express = require('express')
+const pg = require('pg')
+const cors = require('cors')
+
+
+const app = express()
+// configs come from standard PostgreSQL env vars
+// https://www.postgresql.org/docs/9.6/static/libpq-envars.html
+const pool = new pg.Pool()
+
+const queryHandler = (req, res, next) => {
+  pool.query(req.sqlQuery).then((r) => {
+    return res.json(r.rows || [])
+  }).catch(next)
+}
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Welcome to EQ Works 😎')
+})
+
+app.get('/events/hourly', (req, res, next) => {
+  console.log('database', process.env['PGDATABASE'])
+  req.sqlQuery = `
+    SELECT date, hour, events, poi.name as location
+    FROM public.hourly_events
+    JOIN public.poi ON public.poi.poi_id = public.hourly_events.poi_id
+    ORDER BY date, hour
+    LIMIT 168;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/events/daily', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, SUM(events) AS events
+    FROM public.hourly_events
+    GROUP BY date
+    ORDER BY date
+    LIMIT 7;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/stats/hourly', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, hour, impressions, clicks, revenue, poi.name as location
+    FROM public.hourly_stats
+    JOIN public.poi ON public.poi.poi_id = public.hourly_stats.poi_id
+    ORDER BY date, hour
+    LIMIT 168;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/stats/daily', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date,
+        SUM(impressions) AS impressions,
+        SUM(clicks) AS clicks,
+        SUM(revenue) AS revenue
+    FROM public.hourly_stats
+    GROUP BY date
+    ORDER BY date
+    LIMIT 7;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/poi', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT *
+    FROM public.poi;
+  `
+  return next()
+}, queryHandler)
+
+app.listen(process.env.PORT || 5555, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  } else {
+    console.log(`Running on ${process.env.PORT || 5555}`)
+  }
+})
+
+// last resorts
+process.on('uncaughtException', (err) => {
+  console.log(`Caught exception: ${err}`)
+  process.exit(1)
+})
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)
+  process.exit(1)
+})
