1bf20b9ef8ff0c6f83b36d3c1c52ba04cb628fa4
Tammie Chung <tammie.chung11@gmail.com>
2020-01-10 12:08:50 -0500

    Implement basic rate limiter based on 20 requests per minute

--- api/rate-limiter.js
+++ api/rate-limiter.js
@@ -1,6 +1,19 @@
+let counter = 0;
+
+setInterval(() => {
+  counter = 0;
+}, 60000);
+
 const rateLimiter = (req, res, next) => {
-  console.log('using rate-limiter middleware')
-  return next();
+
+  counter++;
+
+  if (counter > 20) {
+    return res.status(403).send(`We're experiencing heavy traffic, please refresh the page and try again!`)
+  } else {
+    return next();
+  }
+
 }
 
 module.exports = {rateLimiter};
